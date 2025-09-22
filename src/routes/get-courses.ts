bbb83d28b74786f297948dacfa74b8736ab88907
FelipeDuan <felipeduan1@gmail.com>
2025-09-21 23:21:12 -0300

    feat: paginacao

--- src/routes/get-courses.ts
+++ src/routes/get-courses.ts
@@ -1,8 +1,8 @@
 import { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
 import { db } from "../database/client.ts";
 import { courses } from "../database/schema.ts";
-import z from "zod";
-import { ilike, asc } from "drizzle-orm";
+import z, { number } from "zod";
+import { ilike, asc, and, SQL } from "drizzle-orm";
 
 export const getCoursesRoute: FastifyPluginAsyncZod = async (server) => {
   server.get(
@@ -14,6 +14,7 @@ export const getCoursesRoute: FastifyPluginAsyncZod = async (server) => {
         querystring: z.object({
           search: z.string().optional(),
           orderBy: z.enum(["id", "title"]).optional().default("id"),
+          page: z.coerce.number().optional().default(1),
         }),
         response: {
           200: z.object({
@@ -23,20 +24,32 @@ export const getCoursesRoute: FastifyPluginAsyncZod = async (server) => {
                 title: z.string(),
               })
             ),
+            total: number(),
           }),
         },
       },
     },
     async (req, reply) => {
-      const { search, orderBy } = req.query;
+      const { search, orderBy, page } = req.query;
 
-      const result = await db
-        .select({ id: courses.id, title: courses.title })
-        .from(courses)
-        .where(search ? ilike(courses.title, `%${search}%`) : undefined)
-        .orderBy(asc(courses[orderBy]));
+      const conditions: SQL[] = [];
 
-      return reply.send({ courses: result });
+      if (search) {
+        conditions.push(ilike(courses.title, `%${search}%`));
+      }
+
+      const [result, total] = await Promise.all([
+        db
+          .select({ id: courses.id, title: courses.title })
+          .from(courses)
+          .where(and(...conditions))
+          .limit(2)
+          .offset((page - 1) * 2)
+          .orderBy(asc(courses[orderBy])),
+        db.$count(courses, and(...conditions)),
+      ]);
+
+      return reply.send({ courses: result, total });
     }
   );
 };
