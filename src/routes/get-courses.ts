0d3a9d8e88bfaaabb881b0eabba2c10b5555f0f2
FelipeDuan <felipeduan1@gmail.com>
2025-09-21 22:48:16 -0300

    feat: add search params

--- src/routes/get-courses.ts
+++ src/routes/get-courses.ts
@@ -2,6 +2,7 @@ import { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
 import { db } from "../database/client.ts";
 import { courses } from "../database/schema.ts";
 import z from "zod";
+import { ilike, asc } from "drizzle-orm";
 
 export const getCoursesRoute: FastifyPluginAsyncZod = async (server) => {
   server.get(
@@ -10,6 +11,10 @@ export const getCoursesRoute: FastifyPluginAsyncZod = async (server) => {
       schema: {
         tags: ["cursos"],
         summary: "Listar todos os cursos",
+        querystring: z.object({
+          search: z.string().optional(),
+          orderBy: z.enum(["id", "title"]).optional().default("id"),
+        }),
         response: {
           200: z.object({
             courses: z.array(
@@ -23,9 +28,13 @@ export const getCoursesRoute: FastifyPluginAsyncZod = async (server) => {
       },
     },
     async (req, reply) => {
+      const { search, orderBy } = req.query;
+
       const result = await db
         .select({ id: courses.id, title: courses.title })
-        .from(courses);
+        .from(courses)
+        .where(search ? ilike(courses.title, `%${search}%`) : undefined)
+        .orderBy(asc(courses[orderBy]));
 
       return reply.send({ courses: result });
     }
